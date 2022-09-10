49c216979d0096344993c73eb301d4dd4485777a
wanxiaolong <wanxiaolong100@163.com>
2022-09-10 17:31:49 +0800

    add cors config

--- FirstService/src/main/java/com/demo/first/controller/FirstController.java
+++ FirstService/src/main/java/com/demo/first/controller/FirstController.java
@@ -9,4 +9,24 @@ public class FirstController {
     public String message() {
         return "Hello First Service";
     }
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable("id") String id) {
+        return "GET: " + id;
+    }
+
+    @PostMapping("/{id}")
+    public String post(@PathVariable("id") String id) {
+        return "POST: " + id;
+    }
+
+    @PutMapping("/{id}")
+    public String put(@PathVariable("id") String id) {
+        return "PUT: " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") String id) {
+        return "DELETE: " + id;
+    }
 }
